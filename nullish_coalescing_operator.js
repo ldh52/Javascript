8d57848585056ea6f7068c97f14455da8a8e1518
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-10-13 21:05:02 +0900

    Create nullish_coalescing_operator.js

---
+++ nullish_coalescing_operator.js
@@ -0,0 +1,20 @@
+// null 병합 연산자 ??
+const example1 = null ?? 'I'; // I
+const example2 = undefined ?? 'love'; // love
+const example3 = 'Codeit' ?? 'JavaScript'; // Codeit
+
+console.log(example1, example2, example3); // I love Codeit
+
+
+// OR 연산자(||)와 비교
+const title1 = false || 'codeit';
+const title2 = false ?? 'codeit';
+
+console.log(title1); // codeit
+console.log(title2); // false
+
+const width1 = 0 || 150;
+const width2 = 0 ?? 150;
+
+console.log(width1); // 150
+console.log(width2); // 0
