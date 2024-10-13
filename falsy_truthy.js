cb6c8d3210499527bc129a8ca5c8cf56370cb6e3
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-10-13 21:14:38 +0900

    Create falsy_truthy.js

---
+++ falsy_truthy.js
@@ -0,0 +1,15 @@
+// falsy
+Boolean(false);
+Boolean(null);
+Boolean(undefined);
+Boolean(0);
+Boolean(NaN);
+Boolean('');
+
+// truthy
+Boolean(true);
+Boolean('codeit');
+Boolean(123);
+Boolean(-123);
+Boolean({});
+Boolean([]);
