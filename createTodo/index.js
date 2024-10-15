3d0d1be0b4955ebe55ffc292f01f36ed14665049
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-10-15 21:15:15 +0900

    Update index.js

--- createTodo/index.js
+++ createTodo/index.js
@@ -1,15 +1,38 @@
-const toDoList = document.querySelector('#to-do-list');
-const items = toDoList.children;
-
-// 1. updateToDo 함수를 완성해 주세요
-function updateToDo(event) {
-  event.target.classList.toggle('done');
+function createTodo(value, order) {
+  const list = document.querySelector('.list');
+  const node = document.createElement('li');
+  if (value.isClear) {
+    node.classList.add('item', 'done');
+  } else {
+    node.classList.add('item');
+  }
+  node.textContent = `${order}. ${value.title}`;
+  list.appendChild(node);
 }
 
-// 2. 반복문을 활용해서 각 li태그에 이벤트 핸들러를 등록해 주세요
-for (let item of items) {
-  item.addEventListener('click', updateToDo);
-}
+const data = [
+  {
+    title: '자바스크립트 공부하기',
+    isClear: true,
+  },
+  {
+    title: '쓰레기 분리수거',
+    isClear: false,
+  },
+  {
+    title: '고양이 밥주기',
+    isClear: true,
+  },
+  {
+    title: '독서하기',
+    isClear: false,
+  },
+  {
+    title: '영어 공부하기',
+    isClear: false,
+  },
+];
 
-// 테스트 코드
-items[2].removeEventListener('click', updateToDo);
+data.forEach((value, i) => {
+  createTodo(value, i + 1);
+});
