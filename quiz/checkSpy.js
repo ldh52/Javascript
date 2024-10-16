ce795aec1b5833a37873d4fe893624618001f678
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-10-16 17:05:25 +0900

    Create checkSpy.js

---
+++ quiz/checkSpy.js
@@ -0,0 +1,49 @@
+const spait = [
+  { codeName: 'ApplePie', members: ['스파이', '스파이', '스파이', '스파이', '스파이'] },
+  { codeName: 'BigBoss', members: ['스파이', '스파이', '스과이', '스파이', '스파이'] },
+  { codeName: 'CEO', members: ['스파이', '스파이', '스파이', '습하이', '스파이'] },
+  { codeName: 'DeathNote', members: ['스파이', '스파이', '스파이', '스파이', '스파이'] },
+  { codeName: 'EarlyBird', members: ['스파이', '스마이', '스파이', '스파이', '스파이'] },
+  { codeName: 'Faker', members: ['스파이', '스파이', '스파이', '스파이', '스파이'] },
+];
+
+function checkSpy(team) {
+  const { codeName, members } = team;
+  const result = members.every((member) => member === '스파이');
+  const message = result
+    ? `팀 ${codeName} 에는 이중 스파이가 없습니다.`
+    : `[주의!] 팀 ${codeName} 에 이중 스파이가 있습니다!`;
+  console.log(message);
+}
+
+// function checkSpy(team) {
+//   const { codeName, members } = team;
+//   const result = members.some((member) => member !== '스파이');
+//   const message = result
+//     ? `[주의!] 팀 ${codeName} 에 이중 스파이가 있습니다!`
+//     : `팀 ${codeName} 에는 이중 스파이가 없습니다.`;
+//   console.log(message);
+// }
+
+// function checkSpy(team) {
+//   const result = team.members.some((member) => member !== '스파이');
+//   const message = result
+//     ? `[주의!] 팀 ${team.codeName} 에 이중 스파이가 있습니다!`
+//     : `팀 ${team.codeName} 에는 이중 스파이가 없습니다.`;
+//   console.log(message);
+// }
+
+// function checkSpy(team) {
+//   const result = team.members.some((member) => member !== '스파이');
+//   let message = '';
+//   if (result) {
+//     message = `[주의!] 팀 ${team.codeName} 에 이중 스파이가 있습니다!`;
+//   } else {
+//     message = `팀 ${team.codeName} 에는 이중 스파이가 없습니다.`;
+//   }
+//   console.log(message);
+// }
+
+
+// 테스트 코드
+spait.forEach((team) => checkSpy(team));
