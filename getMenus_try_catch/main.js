80e036c49b166d7f9e42ff075690d1703290125a
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-10-17 00:05:52 +0900

    Update main.js

--- getMenus_try_catch/main.js
+++ getMenus_try_catch/main.js
@@ -13,3 +13,8 @@ if (menus) {
   const randomMenu = randomElement(menus);
   console.log(`오늘의 랜덤 메뉴는 ${randomMenu.name}입니다!`);
 }
+
+
+// 메뉴 고르는 중...
+// 데이터를 가져오지 못했습니다 :(
+// getMenus() 함수가 끝났습니다.
