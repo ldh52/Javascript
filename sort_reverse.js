a1f7ff0696578c696ebb1a21a8f942702c055d09
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-10-16 17:16:23 +0900

    Update and rename sort.js to sort_reverse.js

--- sort_reverse.js
+++ sort_reverse.js
@@ -19,3 +19,15 @@ console.log(numbers); // (5) [1, 4, 10, 21, 36000]
 // 내림차순 정렬
 numbers.sort((a, b) => b - a);
 console.log(numbers); // (5) [36000, 21, 10, 4, 1]
+
+
+
+
+const letters = ['a', 'c', 'b'];
+const numbers = [421, 721, 353];
+
+letters.reverse();
+numbers.reverse();
+
+console.log(letters); // (3) ["b", "c", "a"]
+console.log(numbers); // (3) [353, 721, 421]
