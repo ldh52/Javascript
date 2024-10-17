57d0a7c9932484a459271cac31d67ac956d0b0d7
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-10-17 16:34:05 +0900

    Create api.js

---
+++ axios/api.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://learn.codeit.kr/api',
+});
+
+export async function getAvatars(params = {}) {
+  const res = await instance.get('/avatars', {
+    params,
+  });
+  return res.data;
+}
+
+export async function getAvatar(id) {
+  const res = await instance.get(`/avatars/${id}`);
+  return res.data;
+}
+
+export async function createAvatar(avatarData) {
+  const res = await instance.post('/avatars', avatarData);
+  return res.data;
+}
+
+export async function patchAvatar(id, avatarData) {
+  const res = await instance.patch(`/avatars/${id}`, avatarData);
+  return res.data;
+}
+
+export async function deleteAvatar(id) {
+  const res = await instance.delete(`/avatars/${id}`);
+  return res.data;
+}
