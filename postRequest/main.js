944d5ff7e0a5634414c39dc5eca16fcf9b246f18
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-10-17 15:28:06 +0900

    Create main.js

---
+++ postRequest/main.js
@@ -0,0 +1,17 @@
+const avatarData = {
+  hairType: 'short2',
+  hairColor: 'brown',
+  skin: 'tone200',
+  clothes: 'hoodie',
+  accessories: 'earbuds',
+};
+
+const res = await fetch('https://learn.codeit.kr/api/avatars', {
+  method: 'POST',
+  body: JSON.stringify(avatarData),
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+const data = await res.json();
+console.log(data);
